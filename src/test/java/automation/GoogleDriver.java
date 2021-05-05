774531dd77bb26e71f6a6a8e96e75508db6f6494
Pablo <pablobaiz12@gmail.com>
2021-05-04 22:34:15 -0300

    deploy two

--- src/test/java/automation/GoogleDriver.java
+++ src/test/java/automation/GoogleDriver.java
@@ -1,6 +1,8 @@
 package automation;
 
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
@@ -9,10 +11,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleDriver {
 	
 	private WebDriver driver;
+	By selectClass = By.className("wpcf7-not-valid-tip");
 
 	@Before
 	public void setUp() {
@@ -21,26 +26,42 @@ public class GoogleDriver {
 		
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.consultoriaglobal.com.ar/cgweb/?page_id=370");
+		driver.get("https://www.consultoriaglobal.com.ar/cgweb");
 		
 		}
 	
 	@Test
 	public void testGooglePage() {
+
 		
-		WebElement searchbox = driver.findElement(By.name("your-email"));
+		WebElement 
+		
+		searchbox = driver.findElement(By.id("menu-item-1364"));
+		searchbox.click();
+		
+		
+		searchbox = driver.findElement(By.name("your-email"));
 		
 		
 		searchbox.clear();
 		searchbox.sendKeys("email false");
-		searchbox.submit();
 		
 		searchbox = driver.findElement(By.name("your-name"));
 		
 		
 		searchbox.clear();
 		searchbox.sendKeys("name false");
+		
+		searchbox = driver.findElement(By.name("your-subject"));
+		searchbox.sendKeys("blabla");
+		
+		
 		searchbox.submit();
+		
+		WebDriverWait wait = new WebDriverWait(driver,4);
+		wait.until(ExpectedConditions.presenceOfElementLocated(selectClass));
+		
+		assertEquals("email is valid",driver.findElement(selectClass).getText());
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
